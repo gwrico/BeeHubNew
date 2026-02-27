390d6c85856267567390e389d106dda03f72bc21
gwrico <cv.anbgroup@gmail.com>
2026-02-28 04:03:28 +0700

    Update SimpleGUI_BeeHub.lua

--- SimpleGUI_BeeHub.lua
+++ SimpleGUI_BeeHub.lua
@@ -3274,7 +3274,7 @@ function SimpleGUI:CreateWindow(options)
     MinimizedIcon.Text = "B"
     MinimizedIcon.TextColor3 = theme.Accent
     MinimizedIcon.BackgroundTransparency = 1
-    MinimizedIcon.TextSize = 50 * scale
+    MinimizedIcon.TextSize = 64 * scale
     MinimizedIcon.Font = Enum.Font.GothamBlack
     MinimizedIcon.Visible = false
     MinimizedIcon.Parent = self.ScreenGui
