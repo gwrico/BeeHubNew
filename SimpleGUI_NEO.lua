99e98e6911d0b1f12464ce08df48a069cfc1fa6b
gwrico <cv.anbgroup@gmail.com>
2026-02-27 18:13:31 +0700

    Update SimpleGUI_NEO.lua

--- SimpleGUI_NEO.lua
+++ SimpleGUI_NEO.lua
@@ -1,5 +1,5 @@
 -- ==============================================
--- 🎨 SIMPLEGUI v8.0 - Bee FUTURISTIC EDITION
+-- 🎨 SIMPLEGUI v8.0 - BEE FUTURISTIC EDITION
 -- ==============================================
 
 local SimpleGUI = {}
@@ -54,7 +54,7 @@ SimpleGUI.Themes = {
         ButtonHover = Color3.fromRGB(45, 45, 55),
         ButtonGlow = Color3.fromRGB(255, 40, 40, 0.15),
         
-        InputBg = Color3.fromRGB(192, 72, 50),
+        InputBg = Color3.fromRGB(25, 25, 32),
         InputBgFocus = Color3.fromRGB(28, 28, 38),
         InputBorder = Color3.fromRGB(255, 40, 40, 0.3),
         
